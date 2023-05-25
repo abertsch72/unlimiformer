a75db5c1d7ce440308eeef87de8a437857e61d63
Uri Alon <urialon1@gmail.com>
2023-05-25 08:56:07 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -17,7 +17,6 @@
 Fine-tuning the library models for sequence to sequence.
 """
 # You can also adapt this script on your own sequence to sequence task. Pointers for this are left as comments.
-from scipy.stats import boltzmann
 import logging
 import os
 import sys

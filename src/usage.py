fede4bdb6376fdf6dc20b8c12307ea57c7f192e3
Amanda Bertsch <42593540+abertsch72@users.noreply.github.com>
2023-09-25 11:54:03 -0400

    knn_args -> unlimiformer_args

--- src/usage.py
+++ src/usage.py
@@ -67,7 +67,7 @@ class UnlimiformerArguments:
 
 # include these lines in your code somewhere before model training
 def training_addin():
-    if knn_args.test_unlimiformer:
+    if unlimiformer_args.test_unlimiformer:
         unlimiformer_kwargs = {
             'layer_begin': unlimiformer_args.layer_begin, 
             'layer_end': unlimiformer_args.layer_end,
